04697f900fa172fcf026a4b381571f7332c6ce82
audreylamy <audrey.lamy92@gmail.com>
2018-11-18 15:27:52 +0100

    add client

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Room msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Room from './components/Room.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Room
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
